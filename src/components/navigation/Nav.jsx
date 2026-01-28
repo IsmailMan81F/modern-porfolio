6697111577a7f844a05e907773951c549bba80b3
IsmailMan81F <ismailman81fmeg@gmail.com>
2026-01-28 09:23:40 +0100

    add the navigation

---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import './Nav.css'
+import '../../index.css'
+
+export default function Nav() {
+  const links = ["About", "Projects", "Testimonials", "Contact"];
+  const [hoverIndex, setHoverIndex] = useState(null);
+
+  const list = links.map((link, index) => (
+    <li
+      className={`nav-el + ${hoverIndex == index ? "hover" : ""}`}
+      key={index}
+      onMouseEnter={() => setHoverIndex(index)}
+      onMouseLeave={() => setHoverIndex(null)}
+    >
+      <a href={`#${link}`}>{link}</a>
+    </li>
+  ));
+
+  return (
+    <nav className="nav">
+      <ul className="nav-list">{list}</ul>
+    </nav>
+  );
+}
